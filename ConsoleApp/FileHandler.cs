0543d35ff2770432603823ee0e5013f3b6641368
Jensen <118268364+Jensen-3000@users.noreply.github.com>
2023-05-26 10:46:04 +0200

    add: createfile method

---
+++ ConsoleApp/FileHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp
+{
+    internal class FileHandler
+    {
+        public bool CreateFile()
+        {
+            bool success = true;
+
+            try
+            {
+                // Ingen multi \ separator hvis cross platform
+                // C:\Users\UserName
+
+                string myUserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+                // Brug path.Combine i stedet for separator
+                File.Create(Path.Combine(myUserFolder, "Niels.txt"));
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+
+            return success;
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,15 @@
 ﻿using ConsoleApp;
 
-WriteToFile write = new WriteToFile();
-try
-{
-    write.WriteToFileInUserDocuments();
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Couldnt write to file." + ex);
-}
+//WriteToFile write = new WriteToFile();
+//try
+//{
+//    write.WriteToFileInUserDocuments();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("Couldnt write to file." + ex);
+//}
+
+FileHandler fileHandler = new FileHandler();
+
+fileHandler.CreateFile();
