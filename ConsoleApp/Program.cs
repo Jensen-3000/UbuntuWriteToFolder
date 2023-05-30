470da75338286fb70d3696ce5d87bfad9d2855a8
Jensen <118268364+Jensen-3000@users.noreply.github.com>
2023-05-30 09:24:53 +0200

    removed:

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,5 @@
 ﻿using ConsoleApp;
 
-//WriteToFile write = new WriteToFile();
-//try
-//{
-//    write.WriteToFileInUserDocuments();
-//}
-//catch (Exception ex)
-//{
-//    Console.WriteLine("Couldnt write to file." + ex);
-//}
-
 FileHandler fileHandler = new FileHandler();
 
 fileHandler.CreateFile();
